fa6c9eadd83c148c896943adfa7ea2d15c48b54b
GRRNebula2 <pahkina.katainen@protonmail.com>
2026-04-06 11:50:33 +0300

    Taustan clearaus. Fps.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { loadSprite } from "./utils.js";
+import { loadSprite, makeSprite } from "./utils.js";
 
 const container = document.querySelector(".container");
 
@@ -26,6 +26,35 @@ async function main() {
         loadSprite("./assets/3.png"),
         loadSprite("./assets/4.png")
     ]);
+
+    const layer1GameObj = makeSprite(c, layer1, {x: 0, y: -100}, 4);
+    const layer2GameObj = makeSprite(c, layer2, {x: 0, y: -100}, 4);
+    const layer3GameObj = makeSprite(c, layer3, {x: 0, y: -100}, 4);
+    const layer4GameObj = makeSprite(c, layer4, {x: 0, y: -100}, 4); 
+    
+    let dt;
+    let oldTimeStamp = 0;
+    let fps;
+    const debugMode = true;
+
+    function gameLoop(timeStamp) {
+        dt = (timeStamp - oldTimeStamp) / 1000;
+        oldTimeStamp = timeStamp;
+
+        fps = Math.round(1 / dt);
+
+        c.clearRect(0, 0, canvas.width, canvas.height);
+
+        if (debugMode) {
+            c.font = "128px Arial";
+            c.fillStyle = "black";
+            c.fillText(fps, 25, 120);
+        }
+
+        requestAnimationFrame(gameLoop)
+    }
+
+    requestAnimationFrame(gameLoop);
 }
 
 main();
--- src/utils.js
+++ src/utils.js
@@ -2,8 +2,8 @@ export function loadSprite(src) {
     return new Promise((resolve, reject) => {
         const img = new Image();
         img.src = src;
-        img.load = () => resolve(img);
-        img.onerror = (err) => reject();
+        img.onload = () => resolve(img);
+        img.onerror = (err) => reject(err);
     })
 }
 
